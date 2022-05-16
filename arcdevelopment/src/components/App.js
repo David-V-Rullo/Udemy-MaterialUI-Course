24475df5993bdd04ac1acabcf9f23315302f2728
David-V-Rullo <d.v.rullo@gmail.com>
2022-05-16 11:31:21 -0400

    Throw up

---
+++ arcdevelopment/src/components/App.js
@@ -0,0 +1,11 @@
+import Button from '@material-ui/core/button';
+import Header from './ui/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ arcdevelopment/src/components/ui/Header.js
@@ -0,0 +1,16 @@
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>Arc</Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
